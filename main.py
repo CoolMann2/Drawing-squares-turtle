8c71a4b93b881703a6621658568b54de276f3404
CoolMann2 <146185301+CoolMann2@users.noreply.github.com>
2023-09-27 11:23:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+window = turtle.Screen()
+window.bgcolor("white")
+
+turtle_1 = turtle.Turtle()
+turtle_1.shape("turtle")
+turtle_1.color("red")
+turtle_1.right(90)
+turtle_1.forward(100)
+turtle_1.right(90)
+turtle_1.forward(100)
+turtle_1.right(90)
+turtle_1.forward(100)
+turtle_1.right(90)
+turtle_1.forward(200)
+turtle_1.left(90)
+turtle_1.forward(100)
+turtle_1.left(90)
+turtle_1.forward(100)
+turtle_1.left(90)
+turtle_1.forward(200)
+turtle_1.left(90)
+turtle_1.forward(100)
+turtle_1.left(90)
+turtle_1.forward(100)
+turtle_1.left(90)
+turtle_1.forward(200)
+turtle_1.right(90)
+turtle_1.forward(100)
+turtle_1.right(90)
+turtle_1.forward(100)
+turtle_1.right(90)
+turtle_1.forward(100)
+
+
+window.exitonclick()
+
